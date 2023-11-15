c8b1c9222ac2433253524940d0b35c9ea6976601
hahatula <you@example.com>
2023-11-15 11:28:14 +0200

    function fix

--- scripts/index.js
+++ scripts/index.js
@@ -29,30 +29,15 @@ const cardsList = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
-// function getCardElement(data) {
-//   const cardElement = cardTemplate.cloneNode(true);
-//   const cardImage = cardElement.querySelector(".card__image");
-//   cardImage.src = initialCards[i].link;
-//   cardImage.alt = initialCards[i].name;
-//   cardElement.querySelector(".card__title-text").textContent =
-//     initialCards[1].name;
-//   cardsList.prepend(cardElement);
-//}
-
 function getCardElement(data) {
-    const cardElement = cardTemplate.cloneNode(true);
-    const cardImage = cardElement.querySelector(".card__image");
-    cardImage.src = data.link;
-    cardImage.alt = data.name;
-    cardElement.querySelector(".card__title-text").textContent =
-      data.name;
-    cardsList.prepend(cardElement);
-  }
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardElement.querySelector(".card__title-text").textContent = data.name;
+  cardsList.prepend(cardElement);
+}
 
-// for (i = 0; i < initialCards.length; i++) {
-//   getCardElement();
-// }
-
-initialCards.forEach(element => {
-    getCardElement(element);
+initialCards.forEach((element) => {
+  getCardElement(element);
 });
